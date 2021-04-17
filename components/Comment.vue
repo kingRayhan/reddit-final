b9ddfcd586ff3da6c984a91d9e58f8d6189d09cf
KingRayhan <rayhan095@gmail.com>
2021-04-17 19:11:03 +0600

    nested comment show

--- components/Comment.vue
+++ components/Comment.vue
@@ -1,47 +1,48 @@
 <template>
-  <div class="flex w-full mb-2">
-    <!-- voting start -->
-    <div class="vote">
-      <button
-        class="arrow arrow--up-vote"
-        :class="{ 'arrow--up-vote--voted': true }"
-      ></button>
-      <button
-        class="arrow arrow--down-vote"
-        :class="{ 'arrow--down-vote--voted': false }"
-      ></button>
-    </div>
-    <!-- voting end -->
+  <div>
+    <div class="flex w-full mb-2">
+      <!-- voting start -->
+      <div class="vote">
+        <button
+          class="arrow arrow--up-vote"
+          :class="{ 'arrow--up-vote--voted': true }"
+        ></button>
+        <button
+          class="arrow arrow--down-vote"
+          :class="{ 'arrow--down-vote--voted': false }"
+        ></button>
+      </div>
+      <!-- voting end -->
 
-    <div class="mb-4 ml-5">
-      <nuxt-link
-        class="font-bold"
-        :to="{
-          name: 'u-username',
-          params: { username: comment.user.username }
-        }"
-      >
-        u/{{ comment.user.username }}
-      </nuxt-link>
-      <span class="text-sm text-gray-600">
-        145 point(s) ( {{ comment.created_at }} )
-      </span>
-      <p class="text-xl">
-        {{ comment.text }}
-      </p>
+      <div class="mb-4 ml-5">
+        <nuxt-link
+          class="font-bold"
+          :to="{
+            name: 'u-username',
+            params: { username: comment.user.username }
+          }"
+        >
+          u/{{ comment.user.username }}
+        </nuxt-link>
+        <span class="text-sm text-gray-600">
+          145 point(s) ( {{ comment.created_at }} )
+        </span>
+        <p class="text-xl">
+          {{ comment.text }}
+        </p>
 
-      <div>
-        <a href="#" class="mr-2 text-sm font-bold text-red-600">
-          Delete
-        </a>
+        <div>
+          <a href="#" class="mr-2 text-sm font-bold text-red-600">
+            Delete
+          </a>
 
-        <a href="#" class="mr-2 text-sm font-bold text-gray-600">
-          <span>Reply</span>
-        </a>
+          <a href="#" class="mr-2 text-sm font-bold text-gray-600">
+            <span>Reply</span>
+          </a>
+        </div>
       </div>
     </div>
-
-    <div v-if="comment.replies">
+    <div v-if="comment.replies" class="ml-8 border-l-2">
       <comment
         v-for="comment in comment.replies"
         :comment="comment"
